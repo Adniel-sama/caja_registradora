cef8ea279694e1d0582f3e0954dbe890077d0755
Daniel <danielc@live.cl>
2022-07-22 11:05:02 -0400

    caja_registradora.py: boleta construida completa

--- caja_registradora.py
+++ caja_registradora.py
@@ -1,9 +1,35 @@
 import datetime
 
 boleta = {}
-rut cliente = input('Ingrese el rut del cliente para asocialo a sus puntos: ')
+rut_cliente = input('Ingrese el rut del cliente para asocialo a sus puntos: ')
 correo_cliente = input ('ingrese le correo del cliente para su boleta virtual: ' )
-fecha_actual = datatime.date.today()
+fecha_actual = datetime.date.today()
 boleta['rut cliente'] = rut_cliente
 boleta['correo_cliente'] = correo_cliente
 boleta['fecha_actual'] = fecha_actual
+boleta['detalle']=[]
+
+
+print('=====Inicia el ingreso de productos=====: ')
+
+estado = True
+while estado:
+    codigo_producto = input('ingrese el código de producto: ')
+    nombre_producto = input('ingrese el nombre de producto: ')
+    categoria = input ('ingrese la categoría del producto: ')
+    precio = input('ingrese el precio del producto: ')
+    cantidad = input(' ingrese la cantidad de productos: ')
+    detalle = {
+        'codigo_producto': codigo_producto,
+        'nombre_pructo': nombre_producto,
+        'categoria': categoria,
+        'precio': precio,
+        'cantidad': cantidad
+    }
+    boleta.get('detalle').append(detalle)
+    respuesta = input('desea continuar? si/no ')
+    if respuesta == 'no':
+        estado=False
+
+print(boleta)
+
