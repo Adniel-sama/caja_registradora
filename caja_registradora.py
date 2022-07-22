46a90fd6c3523302d8c7cbc8626e6517239d8b96
Daniel <danielc@live.cl>
2022-07-22 10:51:24 -0400

    boleta con datos de cliente y fecha

---
+++ caja_registradora.py
@@ -0,0 +1,9 @@
+import datetime
+
+boleta = {}
+rut cliente = input('Ingrese el rut del cliente para asocialo a sus puntos: ')
+correo_cliente = input ('ingrese le correo del cliente para su boleta virtual: ' )
+fecha_actual = datatime.date.today()
+boleta['rut cliente'] = rut_cliente
+boleta['correo_cliente'] = correo_cliente
+boleta['fecha_actual'] = fecha_actual
