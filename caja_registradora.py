e333b7733d5a14787007509ba78c6fe324471b46
Daniel <danielc@live.cl>
2022-07-22 13:13:32 -0400

    caja_registradora.py: impresion boleta archivo completo

--- caja_registradora.py
+++ caja_registradora.py
@@ -4,7 +4,7 @@ boleta = {}
 rut_cliente = input('Ingrese el rut del cliente para asocialo a sus puntos: ')
 correo_cliente = input ('ingrese le correo del cliente para su boleta virtual: ' )
 fecha_actual = datetime.date.today()
-boleta['rut cliente'] = rut_cliente
+boleta['rut_cliente'] = rut_cliente
 boleta['correo_cliente'] = correo_cliente
 boleta['fecha_actual'] = fecha_actual
 boleta['detalle']=[]
@@ -34,9 +34,18 @@ while estado:
 print(f'Cliente: {boleta.get("rut_cliente")}')
 print(f'Correo: {boleta.get("correo_cliente")}')
 print(f'Fecha: {boleta.get("fecha_actual")}')
+total=0
 
 for producto in boleta.get('detalle'):
     print(f'{producto["codigo_producto"]} {producto["nombre_producto"]} {producto["cantidad"]} {producto["precio"]} {int(producto["cantidad"]) * int(producto["precio"])} ')
+    total+=int(producto["cantidad"])*int(producto["precio"])
+print(f'total a pagar : {total}')
 
-
+with open('boleta.txt', 'w') as archivo:
+    archivo.write(f'Cliente: {boleta.get("rut_cliente")}\n')
+    archivo.write(f'Correo: {boleta.get("correo_cliente")}\n')
+    archivo.write(f'Fecha: {boleta.get("fecha_actual")}\n')
+    for producto in boleta.get('detalle'):
+        archivo.write(f'{producto["codigo_producto"]} {producto["nombre_producto"]} {producto["cantidad"]} {producto["precio"]} {int(producto["cantidad"]) * int(producto["precio"])} \n')
+    archivo.write(f'total a pagar: {total}')
 
